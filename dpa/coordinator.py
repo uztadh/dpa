c63da729d8394cc1324516371c69972133c5c3db
nm <nagamocha@gmail.com>
2022-10-04 17:52:29 +0300

    Define interface for and implement default load-balancer

--- dpa/coordinator.py
+++ dpa/coordinator.py
@@ -1,5 +1,18 @@
-from typing import Protocol
+import random
+from queue import PriorityQueue
+from typing import Protocol, Mapping
 from enum import IntEnum
+
+
+class CoordinatorCloud(Protocol):
+    def add_datastore(self) -> bool:
+        ...
+
+    def remove_datastore(self, cloud_ID: int):
+        ...
+
+    def shutdown(self):
+        ...
 
 
 class ScaleOpt(IntEnum):
@@ -45,22 +58,114 @@ class DefaultAutoScaler(AutoScaler):
             return ScaleOpt.NO_CHANGE
 
 
-class CoordinatorCloud(Protocol):
-    def add_datastore(self) -> bool:
+class LoadBalancer(Protocol):
+    def balance_load(
+        self,
+        shards: set[int],
+        servers: set[int],
+        shard_loads: Mapping[int, int],
+        current_locations: Mapping[int, int],
+    ) -> dict[int, int]:
+        """
+        current_locations: shard_num -> server_num
+        """
         ...
 
-    def remove_datastore(self, cloud_ID: int):
-        ...
 
-    def shutdown(self):
-        ...
+class DefaultLoadBalancer(LoadBalancer):
+    def __init__(self):
+        self.epsilon_ratio = 5
+
+    def balance_load(
+        self,
+        shards: set[int],
+        servers: set[int],
+        shard_loads: dict[int, int],
+        current_locations: dict[int, int],
+    ) -> dict[int, int]:
+        updated_locations = current_locations.copy()
+        server_loads: dict[int, int] = {s: 0 for s in servers}
+        server_to_shards: dict[int, list[int]] = {s: [] for s in servers}
+
+        for shard in shards:
+            shard_load = shard_loads[shard]
+            server = current_locations[shard]
+            server_loads[server] = server_loads[server] + shard_load
+            server_to_shards[server].append(shard)
+
+        server_min_queue = PriorityQueue(len(servers))
+        server_max_queue = PriorityQueue(len(servers))
+        for server, load in server_loads.items():
+            server_min_queue.put((load, server))
+            server_max_queue.put((-load, server))
+
+        average_load: float = sum(shard_loads.values()) / len(server_loads)
+        epsilon: float = average_load / self.epsilon_ratio
+
+        while not server_max_queue.empty():
+            max_load, overloaded_server = server_max_queue.get(block=False)
+            max_load = -max_load
+            if max_load <= average_load + epsilon:
+                break
+            while True:
+                shards_in_overloaded_server = server_to_shards[overloaded_server]
+                if not any(shard_loads[s] > 0 for s in shards_in_overloaded_server):
+                    break
+                if server_loads[overloaded_server] <= average_load + epsilon:
+                    break
+
+                min_load, underloaded_server = server_min_queue.get(block=False)
+
+                shard_loads_for_shards_in_overloaded_server = {
+                    shard: shard_loads[shard]
+                    for shard in server_to_shards[overloaded_server]
+                }
+
+                most_loaded_shard = max(
+                    shards_in_overloaded_server,
+                    key=lambda k: shard_loads_for_shards_in_overloaded_server[k],
+                )
+                most_loaded_shardload = shard_loads[most_loaded_shard]
+                server_to_shards[overloaded_server].remove(most_loaded_shard)
+                if (
+                    server_loads[underloaded_server] + most_loaded_shardload
+                    <= average_load + epsilon
+                ):
+                    assert updated_locations[most_loaded_shard] == overloaded_server
+                    updated_locations[most_loaded_shard] = underloaded_server
+                    server_loads[overloaded_server] = max_load - most_loaded_shardload
+
+                    server_loads[underloaded_server] = min_load + most_loaded_shardload
+                    print(
+                        f"Shard {most_loaded_shard} transfered from DS {overloaded_server} to DS {underloaded_server}"
+                    )
+                    server_min_queue.put(
+                        (min_load + most_loaded_shardload, underloaded_server)
+                    )
+                else:
+                    server_min_queue.put((min_load, underloaded_server))
+
+        return updated_locations
 
 
 def main():
-    scale = DefaultAutoScaler()
-    usage = {0: 0.1, 1: 0.5, 2: 0.8}
-    res = scale.autoscale(usage)
-    print(res)
+    SEED = 42
+    r = random.Random(SEED)
+    lb = DefaultLoadBalancer()
+
+    num_shards = 9
+    shards = {sh for sh in range(1, num_shards + 1)}
+    shard_loads = {shard: r.randint(1, 10) for shard in shards}
+
+    num_servers = 3
+    servers = {s for s in range(1, num_servers + 1)}
+
+    server_list = list(servers)
+    current_locations = {sh: r.choice(server_list) for sh in shards}
+
+    updated_locations = lb.balance_load(shards, servers, shard_loads, current_locations)
+    print(current_locations)
+    print(updated_locations)
 
 
 if __name__ == "__main__":
